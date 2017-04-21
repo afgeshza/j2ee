781a17126accc7b28b9b158adb56e2c8b3b62124
trainee <trainee>
2017-04-21 15:34:08 +0700

    exercise 5

---
+++ project5-exercise/Servlet/src/info/Info.java
@@ -0,0 +1,15 @@
+package info;
+
+import student.Student;
+
+public class Info {
+	private Student[] data;
+	private String message;
+	
+	public Info(Student[] data, String message) {
+		this.data = data;
+		this.message = message;
+	}
+	
+	
+}
